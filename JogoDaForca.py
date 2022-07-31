c40a83390c7db0923eacfab504eb089e3a63ee91
Nicolas Bueno <ns.bueno@hotmail.com>
2022-07-31 03:00:56 -0300

    Criado boneco para forca
    Criado boneco de "pauzinhos" para ilustrar melhor o jogo da forca.

--- JogoDaForca.py
+++ JogoDaForca.py
@@ -6,6 +6,12 @@ import os
 from time import sleep
 
 #PALAVRAS PARA FORCA
+boneco_forca_6 = '					 0\n			       		/|\ \n			       		/ \ '
+boneco_forca_5 = '					 0\n			       		/|\ \n			       		/   '
+boneco_forca_4 = '					 0\n			       		/|\ '
+boneco_forca_3 = '					 0\n			       		/|  '
+boneco_forca_2 = '					 0\n			       		 |  '
+boneco_forca_1 = '					 0 '
 palavras_forca = list()
 palavras_frutas = ['MACA', 'BANANA', 'MORANGO', 'JACA', 'MANGA', 'PERA', 'BERGAMOTA', 'MAMAO', 'KIWI', 'CARAMBOLA', 'MELANCIA', 'ABACATE', 'ABACAXI', 'ACAI', 'ACEROLA', 'AMORA', 'CAQUI', 'CEREJA', 'FRAMBOESA', 'FIGO', 'GOIABA', 'JABUTICABA', 'LARANJA', 'LIMAO', 'MARACUJA', 'MELAO', 'MORANGO', 'PESSEGO', 'PITANGA', 'PITAYA', 'UVA']
 palavras_animais = ['CACHORRO', 'GATO', 'GALINHA', 'PORCO', 'BOI', 'VACA', 'MACACO', 'COELHO', 'JAVALI', 'LEAO', 'LEOPARDO', 'PEIXE', 'TUBARAO', 'GOLFINHO', 'ELEFANTE', 'GIRAFA', 'TARTARUGA', 'COBRA', 'TIGRE', 'LEBRE', 'CAPIVARA', 'VEADO', 'HIPOPOTAMO', 'GUAXINIM', 'GAMBA', 'FOCA', 'BALEIA', 'RATO']
@@ -37,13 +43,13 @@ while jogar_novamente == 'S':
         print('Legal, você escolheu animais!!\n')
     elif tipo_de_palavras == 3:
         palavras_forca.extend(palavras_cidades)
-        print('Legal, você escolheu cidades!!\n')
+        print('Legal, você escolheu cidades!!\nCaso a cidade possua duas palavras utilize o traço "-" para juntar os nomes!\nExemplo "DOIS IRMAOS" será "DOIS-IRMAOS"\n')
     else:
         palavras_forca.extend(palavras_frutas)
         print('Você não escolheu uma opção válida :/\nEntão escolhemos frutas para você!\n')
 
     # DEFININDO PALAVRA ALEATÓRIA E OBTENDO NÚMERO DE LETRAS - CRIAÇÃO DE VARIÁVEIS
-    indice_aleatório = random.randint(0, 30)
+    indice_aleatório = random.randint(0, len(palavras_forca) - 1)
     palavra = palavras_forca[indice_aleatório]
     qtde_letras = len(palavra)
     lista_palavra = palavra
@@ -70,10 +76,25 @@ while jogar_novamente == 'S':
         print(f'Você só pode errar {erros_faltantes} vezes até acertar a palavra!')
         print('\n')
         print(''.join(palavra_mostrada))
-        print('\n')
         if erros_faltantes < 6:
-            print('Letras erradas: ', ' '.join(letras_erradas))
             print('\n')
+            print('Letras erradas: ', ' '.join(letras_erradas))
+
+        # DEFININDO QUAL BONECO IRÁ SER MOSTRADO NA TELA    
+        if erros_faltantes == 6:
+            print(boneco_forca_6)
+        elif erros_faltantes == 5:
+            print(boneco_forca_5)
+        elif erros_faltantes == 4:
+            print(boneco_forca_4)
+        elif erros_faltantes == 3:
+            print(boneco_forca_3)
+        elif erros_faltantes == 2:
+            print(boneco_forca_2)
+        else:
+            print(boneco_forca_1)
+        
+        print('\n')
         letra = input('Escolha uma letra ou caso saiba a palavra digite "*" (asterisco): ').upper()
         print('\n')
 
